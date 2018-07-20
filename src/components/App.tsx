700979d68fe932a16a5a387a0bfa9e23a1ce22bb
Prichmondo <Riccardo.Volpato@ding.lan>
2018-07-20 17:16:29 +0100

    added some css styles for AddToBalance

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -34,11 +34,21 @@ class App extends React.Component<{}, IState> {
     );
   }
 
+  private setCustomerView(view: CustomerView): void {
+    this.setState({
+      customerView: view
+    });
+  }
+
+  private addToBalance(value: number): void {
+    console.log("addToBalance", value);
+  }
+
   private getCustomerView(): JSX.Element {
     switch(this.state.customerView) {
       default:
       case CustomerView.AddToBalance:
-        return <AddToBalance selectedCustomer={this.state.selectedCustomer} />;
+        return <AddToBalance onClick={this.addToBalance} />;
       case CustomerView.PayBalance:
         return <span>PayBalance</span>;
     }
@@ -46,12 +56,28 @@ class App extends React.Component<{}, IState> {
 
   private getTabs(): JSX.Element {
     return (
-      <div>
+      <div className="customer-box">
         <ul className="nav nav-tabs">
-          <li><a href="#">Add</a></li>
-          <li><a href="#">Pay</a></li>
+          <li className="nav-item">
+            <a
+              href="#"  
+              className={`nav-link ${this.state.customerView === CustomerView.AddToBalance ? "active" : ""}`} 
+              onClick={()=> this.setCustomerView(CustomerView.AddToBalance)}
+            >
+              Add
+            </a>
+            </li>
+          <li className="nav-item">
+          <a
+              href="#"  
+              className={`nav-link ${this.state.customerView === CustomerView.PayBalance ? "active" : ""}`} 
+              onClick={()=> this.setCustomerView(CustomerView.PayBalance)}
+            >Pay</a>
+            </li>
         </ul>
-        {this.getCustomerView()}
+        <div className="customer-box-container">
+          {this.getCustomerView()}
+        </div>
       </div>
     );
   }
--- src/components/addToBalance.tsx
+++ src/components/addToBalance.tsx
@@ -1,16 +1,31 @@
 import * as React from 'react';
-import './header.scss';
-import ICustomer from '../models/customer';
 
 interface IProps {
-  selectedCustomer?: ICustomer
+  onClick: (value: number)=>void  
 }
 
 class AddToBalance extends React.Component<IProps> {
+
   public render() {
+    const numbers = [1,2,5];
     return (
       <div className="add-to-balance">
-        add to balance
+        <div className="buttons-container">
+          <div className="row">
+            {numbers.map((n,i)=>(
+              <div key={i} className="col">
+                <button
+                  className="btn btn-default btn-lg btn-block" 
+                  onClick={()=>this.props.onClick(n)}>
+                  € {n}
+                </button>
+              </div>
+            ))}
+
+          </div>
+        </div>
+        <hr />
+        <div>custom value</div>
       </div>
     );
   }
