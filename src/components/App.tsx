6c9da909be68dee6a8b4e0d623da34d2f1bddb9d
Prichmondo <Riccardo.Volpato@ding.lan>
2018-07-20 11:07:26 +0100

    created project folders

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        this is the app component...
+      </div>
+    );
+  }
+}
+
+export default App;
