8227ed6061ede2b0e0f0f871de4e955c8dae16f9
Prichmondo <Riccardo.Volpato@ding.lan>
2018-07-20 14:39:22 +0100

    added header comoponent and site background image

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
+import Header from './header';
 import * as React from 'react';
 import './App.scss';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <button className="btn btn-primary">test</button>
+      <div className="container">
+        <Header />
+        
       </div>
     );
   }
---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './header.scss';
+
+class Header extends React.Component {
+  public render() {
+    return (
+      <header className="main-header">
+        <div className="logo" />
+      </header>
+    );
+  }
+}
+
+export default Header;
