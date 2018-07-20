1ac269171dde27c70785d027fbaa42432ab1aa62
Prichmondo <Riccardo.Volpato@ding.lan>
2018-07-20 15:26:50 +0100

    added some components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,33 @@
 import Header from './header';
 import * as React from 'react';
 import './App.scss';
+import ICustomer from '../models/customer'; 
 
-class App extends React.Component {
+interface IState {
+  selectedCustomer?: ICustomer;
+}
+
+class App extends React.Component<{}, IState> {
+
+  constructor(props: {}){
+    super(props);
+    this.state = {}
+  }
+
   public render() {
     return (
       <div className="container">
-        <Header />
-        
+        <Header selectedCustomer={this.state.selectedCustomer} />
+        {this.getView()}    
       </div>
     );
+  }
+
+  private getView(): JSX.Element {
+    
+    return !this.state.selectedCustomer 
+      ? <span>search</span>
+      : <span>Add to Balance</span>
   }
 }
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import './header.scss';
+import ICustomer from '../models/customer';
 
-class Header extends React.Component {
+interface IProps {
+  selectedCustomer?: ICustomer
+}
+
+class Header extends React.Component<IProps> {
   public render() {
+    const customer = this.props.selectedCustomer;
     return (
       <header className="main-header">
         <div className="logo" />
+        <div className="customer-data">
+          {customer ? <span>{customer.email} | balance: $ {customer.balance}</span> : null}
+        </div>
       </header>
     );
   }
